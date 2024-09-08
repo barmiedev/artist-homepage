6fd8485a9bb948e291e81d67a0643bb215c7bd39
barmiedev <bartek@barmie.dev>
2024-09-08 22:46:19 +0200

    ✨ Added Sanity integration
    Configured with environment variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import sanity from '@sanity/astro';
+
+import { loadEnv } from 'vite';
+const {
+  PUBLIC_SANITY_DATASET,
+  PUBLIC_SANITY_PROJECT_ID,
+  SANITY_STUDIO_BASEPATH,
+} = loadEnv(process.env.NODE_ENV || '', process.cwd(), '');
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false, // building static site, so don't use CDN
+      apiVersion: '2024-09-08',
+      studioBasePath: SANITY_STUDIO_BASEPATH,
+    }),
+    react(),
+  ],
+});
