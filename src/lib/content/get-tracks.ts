713f4b534ad0afea9dcedc5cb312cbf0591208f5
barmiedev <bartek@barmie.dev>
2024-09-10 23:40:29 +0200

    💚 Fixed building problems
    Missing types and warning from imports

--- src/lib/content/get-tracks.ts
+++ src/lib/content/get-tracks.ts
@@ -1,6 +1,5 @@
 import { loadQuery } from '@/sanity/lib/load-query';
 import type { SanityDocument } from '@sanity/client';
-import type { QueryParams } from 'sanity';
 
 export const getTracks = async () => {
   const { data } = (await loadQuery({
