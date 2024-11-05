b2bd4c5213e2ddabb7591b3125a9630931cbddfd
barmiedev <bartek@barmie.dev>
2024-11-05 15:58:58 +0100

    ✨ Added simple newsletter form
    Handled with hybrid application output, with Netlify adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,4 @@
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 import { defineConfig } from 'astro/config';
@@ -11,6 +12,8 @@ const {
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: netlify(),
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pl', 'de'],
