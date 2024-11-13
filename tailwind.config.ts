6cbcca89f0170d650760706f0040d87bf0b07f8a
Bartek Mieżyński <112986294+barmiedev@users.noreply.github.com>
2024-11-13 21:44:48 +0100

    ➕ Added Tailwind (#36)
    Added Tailwind as a CSS solution with Typography plugin

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    colors: {
+      background: '#040404',
+      foreground: '#f3f3f3',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Raleway', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
