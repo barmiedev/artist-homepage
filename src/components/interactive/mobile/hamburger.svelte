568faa09ce58c2e0d5db3aa90b83477ef7a05c98
barmiedev <bartek@barmie.dev>
2024-11-18 12:16:56 +0100

    ✨ Added mobile navigation
    In drawer, using Svelte and SVG animation

---
+++ src/components/interactive/mobile/hamburger.svelte
@@ -0,0 +1,50 @@
+<script>
+  const { callback, open } = $props();
+</script>
+
+<button
+  class:open
+  onclick={callback}
+  class="md:hidden ml-auto"
+  aria-label="Toggle menu"
+>
+  <svg width="32" height="24">
+    <line id="top" x1="0" y1="2" x2="32" y2="2" />
+    <line id="middle" x1="12" y1="12" x2="32" y2="12" />
+    <line id="bottom" x1="0" y1="22" x2="32" y2="22" />
+  </svg>
+</button>
+
+<style>
+  svg {
+    min-height: 24px;
+    transition: transform 0.3s ease-in-out;
+    opacity: 1;
+  }
+
+  svg line {
+    @apply stroke-foreground-darken stroke-[4px];
+    transition: transform 0.3s ease-in-out;
+  }
+
+  /* adjust the Z-index, so that the icon is on top of the sidebar */
+  button {
+    z-index: 40;
+  }
+
+  /* rotate the top line */
+  .open #top {
+    transform: translate(6px, 0px) rotate(45deg);
+  }
+
+  /* hide the middle */
+  .open #middle {
+    stroke-width: 0;
+    transform: scale(0);
+  }
+
+  /* rotate the bottom line */
+  .open #bottom {
+    transform: translate(-12px, 9px) rotate(-45deg);
+  }
+</style>
---
+++ src/components/interactive/mobile/navigation.svelte
@@ -0,0 +1,34 @@
+<script>
+  import Hamburger from "./hamburger.svelte";
+
+  const { children } = $props();
+  let open = $state(false);
+  function toggle() {
+    open = !open;
+    document.documentElement.scrollTop = 0;
+    document.documentElement.classList.toggle("overflow-hidden");
+  }
+</script>
+
+<Hamburger callback={toggle} {open} />
+<aside
+  class="md:hidden fixed top-0 w-full h-screen overflow-hidden bg-background shadow-lg sm:hidden"
+  class:open
+>
+  <div class="absolute h-full w-full grid place-items-center">
+    <div class="grid place-items-center gap-12">
+      {@render children?.()}
+    </div>
+  </div>
+</aside>
+
+<style>
+  aside {
+    left: -100%;
+    transition: left 0.3s ease-in-out;
+  }
+
+  .open {
+    left: 0;
+  }
+</style>
