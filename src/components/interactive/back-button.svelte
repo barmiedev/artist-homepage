727ee56148ee8e2fd8cf92a270601c442bd4b6cf
barmiedev <bartek@barmie.dev>
2024-11-18 22:18:43 +0100

    💄 Updated single gig page
    With metadata, image, and others

---
+++ src/components/interactive/back-button.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { ChevronLeft } from "lucide-svelte";
+
+  const { href, children } = $props();
+</script>
+
+<a
+  {href}
+  class="relative group flex items-center transform translate-x-0 select-none mb-8"
+>
+  <ChevronLeft
+    class="absolute w-4 h-4 opacity-0 group-hover:opacity-100 duration-100 text-current"
+  />
+  <span
+    class="absolute group-hover:translate-x-6 translate-x-0 duration-200 ease-in-out text-foreground-darken hover:text-foreground"
+    >{@render children?.()}</span
+  >
+</a>
